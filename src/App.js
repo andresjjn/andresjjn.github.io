9658195227699c45390a030363b9a7bfafb43e38
Andres Jejen Tabares <andres.jejent@perficient.com>
2021-08-11 16:21:07 -0500

    Added QR code and contact us button

--- src/App.js
+++ src/App.js
@@ -8,15 +8,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-          <img src={logo} alt="logo" className="blink-image" />
-            <div className='buttons'>
-              <SocialFollow />  
-            </div>
-            {/* <a href="https://www.flowcode.com/page/ridingforadream?utm_term=dzB1ErnBO#q=dzB1ErnBO&c=f22f7189-569c-49ff-a0f9-c90e37056dc5-SSE:1628634018" title="Scan or click">
-              <img src={qr} alt="Scan or click" className="qr-image"/>
-            </a> */}
-        {/* <img src={coming} alt="Coming soon..." className="coming"/> */}
+        <img src={logo} alt="logo" className="blink-image" />
+          <div className='buttons'>
+            <SocialFollow />  
+          </div>
+        <button className="myButton">
         <a className='contact' href='https://forms.gle/baZy2b2RmtWaXo4ZA'> Contact us </a>
+        </button>
       </header>
     </div>
   );
